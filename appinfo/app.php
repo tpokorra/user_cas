5dfdebd49f61a11a6af451b4541926725189264a
Felix Rupp <kontakt@felixrupp.com>
2016-09-09 14:10:57 +0200

    Fix \OC_User calls

--- appinfo/app.php
+++ appinfo/app.php
@@ -22,8 +22,8 @@
  *
  */
 
-
-
+use \OC_User;
+use \OC_Util;
 
 if (OCP\App::isEnabled('user_cas')) {
 
@@ -32,7 +32,7 @@ if (OCP\App::isEnabled('user_cas')) {
 	OCP\App::registerAdmin('user_cas', 'settings');
 
 	// register user backend
-	\OC\User\User::useBackend( 'CAS' );
+	\OC_User::useBackend( 'CAS' );
 
 	OC::$CLASSPATH['OC_USER_CAS_Hooks'] = 'user_cas/lib/hooks.php';
 	OCP\Util::connectHook('OC_User', 'post_createUser', 'OC_USER_CAS_Hooks', 'post_createUser');
@@ -47,7 +47,7 @@ if (OCP\App::isEnabled('user_cas')) {
 
 			phpCAS::forceAuthentication();
 
-			if (!\OC\User\User::login('', '')) {
+			if (!\OC_User::login('', '')) {
 				$error = true;
 				\OCP\Util::writeLog('cas','Error trying to authenticate the user', \OCP\Util::DEBUG);
 			}
@@ -59,7 +59,7 @@ if (OCP\App::isEnabled('user_cas')) {
 		}
 
 		OC::$REQUESTEDAPP = '';
-		\OC\Util::redirectToDefaultPage();
+		\OC_Util::redirectToDefaultPage();
 	}
 
 
