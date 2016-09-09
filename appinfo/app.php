8c8bcfd6be85e1b31d8fadd9f235a3d024d1a807
Felix Rupp <kontakt@felixrupp.com>
2016-09-09 14:21:01 +0200

    Fix user login

--- appinfo/app.php
+++ appinfo/app.php
@@ -47,7 +47,7 @@ if (OCP\App::isEnabled('user_cas')) {
 
 			phpCAS::forceAuthentication();
 
-			if (!\OC_User::login('', '')) {
+			if (!\OC_User::loginWithCookie('', '')) {
 				$error = true;
 				\OCP\Util::writeLog('cas','Error trying to authenticate the user', \OCP\Util::DEBUG);
 			}
